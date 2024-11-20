dac3c71b0037eb340386a76faed79f51c6b72b22
0xNoushad <choudharynoushad46@gmail.com>
2024-11-21 00:29:22 +0530

    image

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -9,7 +9,7 @@ export const SEO: NextSeoProps = {
     type: "website",
     images: [
       {
-        url: "https://jsoncrack.com/assets/jsoncrack.png",
+        url: "https://imgur.com/a/MYm3dW9",
         width: 1200,
         height: 627,
       },
